3ee5148da32e272351aadc4b336ea4ad81120ddf
Jakub Biesiada <biesiadajakub@icloud.com>
2021-04-06 15:20:28 +0200

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,69 @@
-export function positioning(h: number, v: number): void;
-export function positioning(): void;
-export function positioning(): void;
-export function positioning(): void;
-export function positioning(): void;
-export function positioning(params: any) {
+export type SingleSize = number | string
 
+export type Size =
+  | [all: SingleSize]
+  | [vertical: SingleSize, horizontal: SingleSize]
+  | [top: SingleSize, horizontal: SingleSize, bottom: SingleSize]
+  | [top: SingleSize, right: SingleSize, bottom: SingleSize, left: SingleSize]
+
+const typeSafeSize = (size: SingleSize) => {
+  if (typeof size === 'string') {
+    return parseFloat(size)
+  }
+
+  return size
 }
 
+export function positioning(...params: Size) {
+  switch (params.length) {
+    case 1: {
+      const all = typeSafeSize(params[0])
+
+      return {
+        top: all,
+        right: all,
+        bottom: all,
+        left: all,
+      }
+    }
+
+    case 2: {
+      const vertical = typeSafeSize(params[0])
+      const horizontal = typeSafeSize(params[1])
+
+      return {
+        top: vertical,
+        right: horizontal,
+        bottom: vertical,
+        left: horizontal,
+      }
+    }
+
+    case 3: {
+      const horizontal = typeSafeSize(params[1])
+
+      return {
+        top: typeSafeSize(params[0]),
+        right: horizontal,
+        bottom: typeSafeSize(params[2]),
+        left: horizontal,
+      }
+    }
+
+    case 4: {
+      return {
+        top: typeSafeSize(params[0]),
+        right: typeSafeSize(params[1]),
+        bottom: typeSafeSize(params[2]),
+        left: typeSafeSize(params[3]),
+      }
+    }
+
+    default: {
+      throw new Error('Unsupported position value')
+    }
+  }
+}
 
 // positioning(5, 5)
 // positioning(5, 5, 4)
