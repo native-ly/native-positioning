5205fb9bbc4b74b7f22b649f720e4e888b56a1f5
Jakub Biesiada <biesiadajakub@icloud.com>
2021-07-21 21:32:35 +0200

    :poop:

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -7,81 +7,83 @@ interface TestCase {
   readonly expected: FlexStyle
 }
 
+const testCases: TestCase[] = [
+  {
+    params: [20],
+    expected: {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 20,
+    },
+  },
+  {
+    params: ['102'],
+    expected: {
+      top: '102',
+      right: '102',
+      bottom: '102',
+      left: '102',
+    },
+  },
+  {
+    params: [15, 20],
+    expected: {
+      top: 15,
+      right: 20,
+      bottom: 15,
+      left: 20,
+    },
+  },
+  {
+    params: ['0', '12'],
+    expected: {
+      top: '0',
+      right: '12',
+      bottom: '0',
+      left: '12',
+    },
+  },
+  {
+    params: [16, 33, 24],
+    expected: {
+      top: 16,
+      right: 33,
+      bottom: 24,
+      left: 33,
+    },
+  },
+  {
+    params: [2, 1, 3, 7],
+    expected: {
+      top: 2,
+      right: 1,
+      bottom: 3,
+      left: 7,
+    },
+  },
+  {
+    params: ['3', '3', '3', '2'],
+    expected: {
+      top: '3',
+      right: '3',
+      bottom: '3',
+      left: '2',
+    },
+  },
+  {
+    params: [-1, '000000002', 1_000, 0x1234],
+    expected: {
+      top: -1,
+      right: '000000002',
+      bottom: 1000,
+      left: 4660,
+    },
+  },
+]
+
 describe('positioning', () => {
-  it.each([
-    {
-      params: [20],
-      expected: {
-        top: 20,
-        right: 20,
-        bottom: 20,
-        left: 20,
-      },
-    },
-    {
-      params: ['102'],
-      expected: {
-        top: '102',
-        right: '102',
-        bottom: '102',
-        left: '102',
-      },
-    },
-    {
-      params: [15, 20],
-      expected: {
-        top: 15,
-        right: 20,
-        bottom: 15,
-        left: 20,
-      },
-    },
-    {
-      params: ['0', '12'],
-      expected: {
-        top: '0',
-        right: '12',
-        bottom: '0',
-        left: '12',
-      },
-    },
-    {
-      params: [16, 33, 24],
-      expected: {
-        top: 16,
-        right: 33,
-        bottom: 24,
-        left: 33,
-      },
-    },
-    {
-      params: [2, 1, 3, 7],
-      expected: {
-        top: 2,
-        right: 1,
-        bottom: 3,
-        left: 7,
-      },
-    },
-    {
-      params: ['3', '3', '3', '2'],
-      expected: {
-        top: '3',
-        right: '3',
-        bottom: '3',
-        left: '2',
-      },
-    },
-    {
-      params: [-1, '000000002', 1_000, 0x1234],
-      expected: {
-        top: -1,
-        right: '000000002',
-        bottom: 1000,
-        left: 4660,
-      },
-    },
-  ] as TestCase[])(
+  it.each(testCases)(
     'should create positioning for $params',
     ({ params, expected }) => {
       expect(positioning(...params)).toEqual(expected)
